6662ab50adc5881c226059708ad9e260d4f360ac
Bryan <ing.bryanvasquez@gmail.com>
2016-07-24 01:32:16 -0400

    Some troubles fixed

--- Messenger/App_Start/BundleConfig.cs
+++ Messenger/App_Start/BundleConfig.cs
@@ -17,6 +17,9 @@ namespace Messenger
             bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
                         "~/Scripts/jquery-ui-1.12.0.min.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/jqueryajax").Include(
+                        "~/Scripts/jquery.unobtrusive-ajax.min.js"));
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
--- Messenger/ChatHub.cs
+++ Messenger/ChatHub.cs
@@ -8,9 +8,19 @@ namespace Messenger
 {
     public class ChatHub : Hub
     {
-        public void Hello(string usermessage)
+        public void Hello(string usermessage, string fontweight, string fontcursive, string fontcolor, string fontsize)
         {
-            Clients.All.hello(usermessage);
+            Clients.All.hello(usermessage, fontweight, fontcursive, fontcolor, fontsize);
+        }
+
+        public void Shake(bool active)
+        {
+            Clients.All.shake(active);
+        }
+
+        public void ImageUploaded(bool reload)
+        {
+            Clients.All.imageuploaded(reload);
         }
     }
 }
